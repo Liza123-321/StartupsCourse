efde1c724a7699365533cf26fd8fff084cce486a
Liza123-321 <liza-98k@mail.ru>
2018-05-08 12:19:24 +0300

    added invites db

--- helpers/tempDataToDb.helper.js
+++ helpers/tempDataToDb.helper.js
@@ -1,10 +1,12 @@
 const categories = require('../data/db/categories.json');
 const startups = require('../data/db/startups.json');
 const users = require('../data/db/users.json');
+const invites = require('../data/db/invites.json')
 
 
 export default async (db) => {
     const dbUsers = await db['Users'].bulkCreate(users);
     const dbCategories = await db['Categories'].bulkCreate(categories);
     const dbStartups = await db['Startups'].bulkCreate(startups);
+    const dbInvites = await db['Invites'].bulkCreate(invites);
 };
